187daa864eabe1f1448170338c6b193c8f98a874
Bugs <voov3r@gmail.com>
2021-11-27 20:45:01 +0300

    test upd

--- test/api.test.js
+++ test/api.test.js
@@ -1,5 +1,4 @@
-const supertest = require('supertest');
-const request = supertest('http://localhost:5000');
+const request = require('supertest')('http://localhost:5000');
 
 describe('api tests', () => {
   let id = null;
@@ -7,20 +6,21 @@ describe('api tests', () => {
     const res = await request.get('/person');
     expect(res.statusCode).toBe(200);
   })
-  test('create new person', async () => {
+  test('create new person, status code 201', async () => {
     const newPerson = { name: 'Jack', age: 30, hobbies: 'Bike,Diving' };
     const res = await request.post('/person').send(newPerson);
     id = JSON.parse(res.text).id;
-    expect(res.text).toEqual(JSON.stringify({ id, name: 'Jack', age: 30, hobbies: ['Bike', 'Diving'] }));
+    expect(res.statusCode).toBe(201);
+    expect(JSON.parse(res.text)).toEqual({ id, name: 'Jack', age: 30, hobbies: ['Bike', 'Diving'] });
   })
   test('get person', async () => {
     const res = await request.get('/person/' + id);
-    expect(res.text).toEqual(JSON.stringify({ id, name: 'Jack', age: 30, hobbies: ['Bike', 'Diving'] }));
+    expect(JSON.parse(res.text)).toEqual({ id, name: 'Jack', age: 30, hobbies: ['Bike', 'Diving'] });
   })
   test('update person', async () => {
     newPerson = { name: 'Jack', age: 33, hobbies: 'Bike,Diving,Theater' };
     const res = await request.put('/person/' + id).send(newPerson);
-    expect(res.text).toEqual(JSON.stringify({ id, name: 'Jack', age: 33, hobbies: ['Bike', 'Diving', 'Theater'] }));
+    expect(JSON.parse(res.text)).toEqual({ id, name: 'Jack', age: 33, hobbies: ['Bike', 'Diving', 'Theater'] });
   })
   test('delete person', async () => {
     const res = await request.delete('/person/' + id);
